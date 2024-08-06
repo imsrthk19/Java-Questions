b7f13d9fc0427a29fe2760d02cf3a40b21c0300f
Sarthak Srivastava <145271363+imsrthk19@users.noreply.github.com>
2024-08-06 08:15:32 +0530

    Create ClassWork.java

---
+++ ClassWork.java
@@ -0,0 +1,59 @@
+public class ClassWork {
+    public static void main(String[] args) {
+        int radius = 5;
+        double area = 3.14*radius*radius;
+        System.out.println(area);
+    }
+}
+*****************************************************************************
+public class ClassWork {
+    public static void main(String[] args) {
+        int pri = 2100;
+        int rate=5;
+        int time=2;
+        int si = (pri*rate*time)/100;
+        System.out.println(si);
+    }
+}
+*******************************************************************************
+public class ClassWork {
+    public static void main(String[] args) {
+        int n=6;
+        if (n%2==0) {
+            System.out.print("Even Number");
+        }
+        else
+        {
+            System.out.print("Odd Number");
+        }
+    }
+*******************************************************************************
+public class ClassWork {
+    public static void main(String[] args) {
+        int celsius=0;
+        int conversion = (celsius*(9/5))+32;
+        System.out.println(conversion);
+    }
+*****************************************************************************
+public class ClassWork {
+    public static void main(String[] args) {
+        int celsius=0;
+        int conversion = (celsius*(9/5))+32;
+        System.out.println(conversion);
+    }
+}
+**********************************************************************************
+public class ClassWork {
+    public static void main(String[] args) {
+        int marks =80;
+        if (marks >90){
+            System.out.println("A");
+        }
+            else if(marks < 90 && marks>80){
+                System.out.println("B");
+            }
+            else{
+                System.out.println("Fail");
+            }
+        }
+    }
