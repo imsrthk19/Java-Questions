8cf431f8b30bb2c09e1159571a97dee950b74f3d
Sarthak Srivastava <145271363+imsrthk19@users.noreply.github.com>
2024-08-07 19:57:36 +0530

    Questions HomeWork.java

--- ClassWork.java
+++ ClassWork.java
@@ -1,3 +1,4 @@
+//Area of circle:
 public class ClassWork {
     public static void main(String[] args) {
         int radius = 5;
@@ -6,6 +7,7 @@ public class ClassWork {
     }
 }
 *****************************************************************************
+//Simple Interest:
 public class ClassWork {
     public static void main(String[] args) {
         int pri = 2100;
@@ -16,6 +18,7 @@ public class ClassWork {
     }
 }
 *******************************************************************************
+//Even or Odd Number:
 public class ClassWork {
     public static void main(String[] args) {
         int n=6;
@@ -28,6 +31,7 @@ public class ClassWork {
         }
     }
 *******************************************************************************
+//Temperature Conversion from celsius to Fahrenheit:
 public class ClassWork {
     public static void main(String[] args) {
         int celsius=0;
@@ -35,25 +39,48 @@ public class ClassWork {
         System.out.println(conversion);
     }
 *****************************************************************************
-public class ClassWork {
+//Find greatest among three numbers:
+public class JavaBasics {
     public static void main(String[] args) {
-        int celsius=0;
-        int conversion = (celsius*(9/5))+32;
-        System.out.println(conversion);
+    int a=2;
+    int b=9;
+    int c=8;
+    if(a>b){
+        if(a>c){
+            System.out.println("a is greatest");
+    }
+    else if(b>c){
+        if(b>a){
+        System.out.println("b is greatest");
     }
 }
+    else if (c>a){
+        if (c>b){
+            System.out.println("c is greatest");
+        } 
+    }
+}
+}
+}
 **********************************************************************************
+//Calculating grades:
 public class ClassWork {
     public static void main(String[] args) {
         int marks =80;
         if (marks >90){
             System.out.println("A");
         }
-            else if(marks < 90 && marks>80){
+        else if(marks < 90 && marks > 80){
                 System.out.println("B");
             }
-            else{
-                System.out.println("Fail");
+        else if(marks < 80 && marks > 70){
+            System.out.println("C");
+        else if(marks < 70 && marks > 60){
+            System.out.println("D");
+        else if(marks < 60 && marks > 50){
+            System.out.println("E");
+        else{
+            System.out.println("Fail");
             }
         }
     }
