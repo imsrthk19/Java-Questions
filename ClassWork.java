5b799c8ea949733cbc79b1f5ece23cf240845e2e
Sarthak Srivastava <145271363+imsrthk19@users.noreply.github.com>
2024-08-07 20:25:32 +0530

    Create ClassWork.java

---
+++ ClassWork.java
@@ -0,0 +1,22 @@
+public class Hello{
+    public static void main(String args[]){
+      int a=5, b=10, sum=0;
+      sum = a+b;
+      System.out.print(a + b + "a+b");
+      System.out.println("1");
+      System.out.println("22");
+      System.out.println("333");
+      System.out.println("4444");
+      System.out.print("\n 1 \n 22 \n 333 \n 4444 \n");
+      System.out.print("a,b," + a + ","+b);
+      System.out.print("\n");
+      System.out.print(a + ","+ "a,b," + b);
+      System.out.print("\n");
+      System.out.printf("%d%d,a,b",a,b);
+      System.out.print("\n");
+      System.out.print("ab"+ sum);
+      System.out.print("\n");
+      System.out.print(a + b + "" + "ab");
+      System.out.print("\n");
+    }
+}
